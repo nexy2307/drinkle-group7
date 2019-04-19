09c8a7ab2b06c85c62b1344e2acb90a93a813d23
Vincenzo Buono <vincebshock@gmail.com>
2019-04-19 17:18:54 +0200

    Application main entry point & file structure added

---
+++ src/main/java/com/espressoshock/drinkle/Main.java
@@ -0,0 +1,24 @@
+package com.espressoshock.drinkle;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Text text = new Text("Hello world drinkle!");
+        Group root = new Group();
+        text.setY(50);
+        root.getChildren().add(text);
+        primaryStage.setTitle("Drinkle");
+        primaryStage.setScene(new Scene(root, 1000, 729));
+        primaryStage.show();
+    }
+}
