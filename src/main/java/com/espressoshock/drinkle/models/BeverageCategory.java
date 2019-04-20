73674de87ddf384567f13777d5015f24b433339e
Alex Oachesu <alex.oachesu0001@stud.hkr.se>
2019-04-20 17:32:58 +0200

    new BeverageCategory elements added

--- src/main/java/com/espressoshock/drinkle/models/BeverageCategory.java
+++ src/main/java/com/espressoshock/drinkle/models/BeverageCategory.java
@@ -6,7 +6,9 @@ public enum BeverageCategory implements ICategory {
     //***** MORE TO COME
     NON_ALCOHOLIC("Alcohol-free"),
     CLASSIC("Contemporary Classic"),
-    HOT("Hot drinks"), FROZEN("Frozen drinks");
+    HOT("Hot drinks"), FROZEN("Frozen drinks"), LONG("Long drinks"),
+    SHORT("Short drinks"), SHOT("Shot drinks"), APERITIVE("Aperitive drinks"), DIGESTIVE("Digestive drinks"),
+    ANY_TIME("Any time drinks");
 
     private final String displayName;
 
