843b8a100affd9acdff511f7e0865a58c0d00aaa
Alex Oachesu <alex.oachesu0001@stud.hkr.se>
2019-04-20 19:14:33 +0200

    Country Enum following class diagram

--- src/main/java/com/espressoshock/drinkle/models/Country.java
+++ src/main/java/com/espressoshock/drinkle/models/Country.java
@@ -1,4 +1,31 @@
 package com.espressoshock.drinkle.models;
 
 public enum Country {
+    //TODO ..MORE TO COME
+    AUSTRIA("  ","Austria"), AUSTRALIA("","Australia"),
+    ALGERIA("","Algeria");
+
+    private final String countryCode;
+    private final String countryName;
+
+    Country(String countryCode, String countryName) {
+        this.countryCode = countryCode;
+        this.countryName = countryName;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "countryCode='" + countryCode + '\'' +
+                ", countryName='" + countryName + '\'' +
+                '}';
+    }
 }
