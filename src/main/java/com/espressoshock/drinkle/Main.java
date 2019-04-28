a5fc39153b64601fcba485948483b842c39ec69b
nexy2307 <nexy2307@hotmail.rs>
2019-04-28 21:03:09 +0200

    added icon.png
    changed some code in Main.java
    added icon.png
    small patch on main scene .fxml

--- src/main/java/com/espressoshock/drinkle/Main.java
+++ src/main/java/com/espressoshock/drinkle/Main.java
@@ -3,6 +3,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -23,6 +24,7 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.getIcons().add(new Image("/images/icon.png"));
         primaryStage.show();
     }
 }
