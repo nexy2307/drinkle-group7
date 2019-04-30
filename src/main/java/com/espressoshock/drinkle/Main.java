fd863e71ddb1ac3c99339b1c5855bb7ee4042ee3
Vincenzo Buono <vincebshock@gmail.com>
2019-04-30 08:52:46 +0200

    Font loader added
    Font load with lato font added

--- src/main/java/com/espressoshock/drinkle/Main.java
+++ src/main/java/com/espressoshock/drinkle/Main.java
@@ -3,6 +3,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -14,7 +15,18 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
+        //RESOURCE LOADING //***********
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/wrappers/main-v-wrapper.fxml"));
+        //FONT LOADING //***********
+        Font.loadFont(getClass().getResource("/fonts/Lato-Black.ttf").toExternalForm(), 10);
+        Font.loadFont(getClass().getResource("/fonts/Lato-BlackItalic.ttf").toExternalForm(), 10);
+        Font.loadFont(getClass().getResource("/fonts/Lato-Bold.ttf").toExternalForm(), 10);
+        Font.loadFont(getClass().getResource("/fonts/Lato-BoldItalic.ttf").toExternalForm(), 10);
+        Font.loadFont(getClass().getResource("/fonts/Lato-Italic.ttf").toExternalForm(), 10);
+        Font.loadFont(getClass().getResource("/fonts/Lato-Light.ttf").toExternalForm(), 10);
+        Font.loadFont(getClass().getResource("/fonts/Lato-LightItalic.ttf").toExternalForm(), 10);
+        Font.loadFont(getClass().getResource("/fonts/Lato-Regular.ttf").toExternalForm(), 10);
+
         primaryStage.setTitle("Drinkle");
         primaryStage.setScene(new Scene(root, 1000, 729));
         primaryStage.show();
