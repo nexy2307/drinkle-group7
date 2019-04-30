4331d0fe94896ebf44cbe7e48ba5d61408a555a6
Nemanja Negovanovic <nexy2307@hotmail.rs>
2019-04-30 10:43:07 +0200

    patch MainSceneController.java
    patched MainSceneController.java fixed some warnings

--- src/main/java/com/espressoshock/drinkle/controllers/MainSceneController.java
+++ src/main/java/com/espressoshock/drinkle/controllers/MainSceneController.java
@@ -1,17 +1,13 @@
 package com.espressoshock.drinkle.controllers;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 import javafx.scene.shape.Rectangle;
@@ -35,7 +31,7 @@ public class MainSceneController implements Initializable {
     @FXML
     Rectangle rectangle;
 
-    double initialX,initialY;
+    private double initialX,initialY;
     public void choseIngredient(Event event) {
         Label lbl = (Label) event.getSource();
         String s = lbl.getText();
@@ -47,54 +43,45 @@ public class MainSceneController implements Initializable {
     Button btn = (Button) event.getSource();
     lblHeader.setText(btn.getText()+" button pressed!");
     }
-    public void sliderProgressChange(){
-        slider.valueProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
-                NumberFormat numFormat = NumberFormat.getInstance();
-                numFormat.setMaximumFractionDigits(2);
-                numFormat.setGroupingUsed(false);
-                Double sliderValue = Double.valueOf((double) slider.getValue());
-                sliderValue = Double.valueOf(numFormat.format(sliderValue));
-                Double dbl2 = sliderValue*100;
-                Double dbl3 = dbl2 * 0.0315;
-                progressGlass.setProgress(sliderValue);
+    private void sliderProgressChange(){
+        slider.valueProperty().addListener((arg0, arg1, arg2) -> {
+            NumberFormat numFormat = NumberFormat.getInstance();
+            numFormat.setMaximumFractionDigits(2);
+            numFormat.setGroupingUsed(false);
+            double sliderValue = slider.getValue();
+            sliderValue = Double.valueOf(numFormat.format(sliderValue));
+            Double dbl2 = sliderValue*100;
+            Double dbl3 = dbl2 * 0.0315;
+            progressGlass.setProgress(sliderValue);
 
-                lblVol.setText(String.valueOf(dbl2.intValue()));
-                lblCost.setText(String.valueOf(numFormat.format(dbl3))+"$");
+            lblVol.setText(String.valueOf(dbl2.intValue()));
+            lblCost.setText(numFormat.format(dbl3)+"$");
 
-            }
         });
 
     }
-    public void setGraphic(){
+    private void setGraphic(){
         tab1.setGraphic(new ImageView("images/editorIcon.png"));
         tab2.setGraphic(new ImageView("images/drinkIcon.png"));
     }
     private void addDraggableNode(final Node node) {
 
-        node.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent me) {
-                if (me.getButton() != MouseButton.MIDDLE) {
-                    initialX = me.getSceneX();
-                    initialY = me.getSceneY();
-                }
+        node.setOnMousePressed(me -> {
+            if (me.getButton() != MouseButton.MIDDLE) {
+                initialX = me.getSceneX();
+                initialY = me.getSceneY();
             }
         });
 
-        node.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent me) {
-                if (me.getButton() != MouseButton.MIDDLE) {
-                    node.getScene().getWindow().setX(me.getScreenX() - initialX);
-                    node.getScene().getWindow().setY(me.getScreenY() - initialY);
-                }
+        node.setOnMouseDragged(me -> {
+            if (me.getButton() != MouseButton.MIDDLE) {
+                node.getScene().getWindow().setX(me.getScreenX() - initialX);
+                node.getScene().getWindow().setY(me.getScreenY() - initialY);
             }
         });
     }
     @FXML
-    private void exitProgramAction(Event exitProgramEvent) {
+    private void exitProgramAction() {
         System.exit(0);
     }
     @FXML
@@ -102,7 +89,7 @@ public class MainSceneController implements Initializable {
         Stage stage = (Stage)((ImageView)minimizeProgramEvent.getSource()).getScene().getWindow();
         stage.setIconified(true);
     }
-    public static void clipChildren(Region region, double arc) {
+    private static void clipChildren(Region region, double arc) {
 
         final Rectangle outputClip = new Rectangle();
         outputClip.setArcWidth(arc);
