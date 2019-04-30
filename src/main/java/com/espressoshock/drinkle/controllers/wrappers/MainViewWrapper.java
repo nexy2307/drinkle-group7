d8b4f89abde3a21884f065bd1218780a3a846ae9
nexy2307 <nexy2307@hotmail.rs>
2019-04-30 17:57:01 +0200

    added addDraggableNode method
    implemented draggable node
    created RecipeEditor.fxml

--- src/main/java/com/espressoshock/drinkle/controllers/wrappers/MainViewWrapper.java
+++ src/main/java/com/espressoshock/drinkle/controllers/wrappers/MainViewWrapper.java
@@ -3,31 +3,53 @@ package com.espressoshock.drinkle.controllers.wrappers;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MainViewWrapper {
-
+    private double initialX, initialY;
     @FXML
     private AnchorPane loadingPane;
+    @FXML
+    private Rectangle draggableNode;
 
     @FXML
     private void exitProgramAction() {
         System.exit(0);
     }
     @FXML
-    public void minimizeProgramAction(Event minimizeProgramEvent) {
+    private void minimizeProgramAction(Event minimizeProgramEvent) {
         Stage stage = (Stage)((ImageView)minimizeProgramEvent.getSource()).getScene().getWindow();
         stage.setIconified(true);
+    }
+    private void addDraggableNode(final Node node) {
+
+        node.setOnMousePressed(mouseEvent -> {
+            if (mouseEvent.getButton() != MouseButton.MIDDLE) {
+                initialX = mouseEvent.getSceneX();
+                initialY = mouseEvent.getSceneY();
+            }
+        });
+
+        node.setOnMouseDragged(me -> {
+            if (me.getButton() != MouseButton.MIDDLE) {
+                node.getScene().getWindow().setX(me.getScreenX() - initialX);
+                node.getScene().getWindow().setY(me.getScreenY() - initialY);
+            }
+        });
     }
 
     @FXML
     public void initialize() throws IOException {
         //fields loaded here -> check if logged/remembered is checked etc...
+
         boolean auth = false;
         /*if(auth){
             //logged show main ui
@@ -37,6 +59,7 @@ public class MainViewWrapper {
             this.loadingPane.getChildren().add(authLogin);
 
         }*/
+        addDraggableNode(draggableNode);
     }
 
 }
