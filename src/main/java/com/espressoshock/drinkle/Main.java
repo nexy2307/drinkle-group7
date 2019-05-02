ae521ed38bb6cab5357e8e82df0464d70041b9c8
Vincenzo Buono <vincebshock@gmail.com>
2019-05-02 11:20:00 +0200

    Font list updated
    Font list updated. Lato -> Vegur

--- src/main/java/com/espressoshock/drinkle/Main.java
+++ src/main/java/com/espressoshock/drinkle/Main.java
@@ -3,6 +3,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -14,9 +15,21 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
+        //RESOURCE LOADING //***********
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/wrappers/main-v-wrapper.fxml"));
+        //FONT LOADING //***********
+
+
         primaryStage.setTitle("Drinkle");
         primaryStage.setScene(new Scene(root, 1000, 729));
         primaryStage.show();
+    }
+
+    private void loadFonts(){
+        //VEGUR//***********
+        Font.loadFont(getClass().getResource("/fonts/Vegur-Bold.ttf").toExternalForm(), 10);
+        Font.loadFont(getClass().getResource("/fonts/Vegur-Light.ttf").toExternalForm(), 10);
+        Font.loadFont(getClass().getResource("/fonts/Vegur-Regular.ttf").toExternalForm(), 10);
+        //END VEGUR//***********
     }
 }
