b9ec5b40e5d06e6f34e1fd0c73db9111645a0dbe
Vincenzo Buono <vincebshock@gmail.com>
2019-05-03 09:08:36 +0200

    Window drag added

--- src/main/java/com/espressoshock/drinkle/controllers/wrappers/MainViewWrapper.java
+++ src/main/java/com/espressoshock/drinkle/controllers/wrappers/MainViewWrapper.java
@@ -4,15 +4,43 @@ import static com.espressoshock.drinkle.appState.UserState.loggedIn;
 import static com.espressoshock.drinkle.appState.UserState.loggedOut;
 
 import com.espressoshock.drinkle.appState.Current;
-import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MainViewWrapper {
+
+    /********* =WINDOW          */
+    public static class Window{
+        private static double currentY;
+        private static double currentX;
+
+        public Window() {
+        }
+
+        public static double getCurrentY() {
+            return currentY;
+        }
+
+        public static void setCurrentY(double currentY) {
+            Window.currentY = currentY;
+        }
+
+        public static double getCurrentX() {
+            return currentX;
+        }
+
+        public static void setCurrentX(double currentX) {
+            Window.currentX = currentX;
+        }
+    }
+    /********* END =WINDOW          */
 
     @FXML
     private AnchorPane loadingPane;
@@ -31,8 +59,16 @@ public class MainViewWrapper {
         }
     }
 
+    /********* =WINDOW-CONTROLS */
     @FXML
-    public void closeWindow(Event e){
-        System.exit(0);
+    void windowDragStart(MouseEvent event) {
+        Window.currentX = event.getSceneX();
+        Window.currentY = event.getY();
     }
+    @FXML
+    void windowDragEnd(MouseEvent event) {
+       ((Node) event.getSource()).getScene().getWindow().setX(event.getScreenX() - Window.currentX);
+       ((Node) event.getSource()).getScene().getWindow().setY(event.getScreenY() - Window.currentY);
+    }
+    /********* END =WINDOW-CONTROLS */
 }
