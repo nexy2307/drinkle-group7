8f923b85e91c747fa7f1f9321f7be93fdb798623
Vincenzo Buono <vincebshock@gmail.com>
2019-05-04 02:36:46 +0200

    AuthRegistration folder structure and basic boilerplate added

--- src/main/java/com/espressoshock/drinkle/viewLoader/ViewMetadata.java
+++ src/main/java/com/espressoshock/drinkle/viewLoader/ViewMetadata.java
@@ -1,9 +1,11 @@
 package com.espressoshock.drinkle.viewLoader;
 
 import com.espressoshock.drinkle.controllers.auth.AuthLogin;
+import com.espressoshock.drinkle.controllers.auth.AuthRegistration;
 
 public enum ViewMetadata {
-    AUTH_LOGIN(AuthLogin.class, "/fxml/auth/auth-login.fxml");
+    AUTH_LOGIN(AuthLogin.class, "/fxml/auth/auth-login.fxml"),
+    AUTH_REGISTRATION(AuthRegistration.class, "/fxml/auth/auth-registration.fxml");
 
     private final Class controllerClass;
     private final String resourcePath;
