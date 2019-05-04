61aab5e84a8742341fbd2af2fc513b8dda46c850
Vincenzo Buono <vincebshock@gmail.com>
2019-05-04 10:17:19 +0200

    AuthLogin-MainView connection & all views basic styling added

--- src/main/java/com/espressoshock/drinkle/controllers/auth/AuthLogin.java
+++ src/main/java/com/espressoshock/drinkle/controllers/auth/AuthLogin.java
@@ -1,13 +1,20 @@
 package com.espressoshock.drinkle.controllers.auth;
 
 import com.espressoshock.drinkle.viewLoader.EventDispatcherAdapter;
+import com.espressoshock.drinkle.viewLoader.ViewLoader;
 import com.espressoshock.drinkle.viewLoader.ViewMetadata;
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 
 public class AuthLogin extends EventDispatcherAdapter {
     @FXML
-    void openRegistrationView(MouseEvent event) {
+    public void openRegistrationView(MouseEvent event) {
         super.dispatchViewChangeRequest(ViewMetadata.AUTH_REGISTRATION);
     }
+    @FXML
+    public void login(MouseEvent event){
+        //some serious business logic
+        super.dispatchViewChangeRequest(ViewLoader.default_view);
+    }
+
 }
