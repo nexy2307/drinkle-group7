19cfdd9c7120a47037e886fd101166321cfb9421
Vincenzo Buono <vincebshock@gmail.com>
2019-05-05 06:30:20 +0200

    Sidebar default_loaded view binding added

--- src/main/java/com/espressoshock/drinkle/controllers/app/Sidebar.java
+++ src/main/java/com/espressoshock/drinkle/controllers/app/Sidebar.java
@@ -4,6 +4,8 @@ import com.espressoshock.drinkle.viewLoader.EventDispatcherAdapter;
 import com.espressoshock.drinkle.viewLoader.ViewLoader;
 import com.espressoshock.drinkle.viewLoader.ViewMetadata;
 import javafx.fxml.FXML;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 
 public class Sidebar extends EventDispatcherAdapter {
@@ -13,10 +15,17 @@ public class Sidebar extends EventDispatcherAdapter {
 
     @FXML
     public void initialize() {
+        /********* =DEFAULT_LOADED SELECTOR */
+        this.selecteMenuItem(ViewLoader.default_view.name());
+        /********* END =DEFAULT_LOADED SELECTOR */
+
+        /********* =TOGGLE-GROUP SELECTED STATE LISTENER */
         sidebarMenu.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
             if(sidebarMenu.getSelectedToggle() != null)
                 super.dispatchViewChangeRequest(this.getViewMetadata(sidebarMenu.getSelectedToggle().getUserData().toString()));
         });
+        /********* END =TOGGLE-GROUP SELECTED STATE LISTENER */
+
     }
 
     public ViewMetadata getViewMetadata(String viewName){
@@ -30,5 +39,11 @@ public class Sidebar extends EventDispatcherAdapter {
 
     public String getSelectedMenuItem(){
        return (String)sidebarMenu.getSelectedToggle().getUserData().toString();
+    }
+    public void selecteMenuItem(String userData){
+        for(Toggle toggle : this.sidebarMenu.getToggles())
+            if(toggle.getUserData().toString().equals(userData))
+                toggle.setSelected(true);
+            //careful here-userData must be valid
     }
 }
