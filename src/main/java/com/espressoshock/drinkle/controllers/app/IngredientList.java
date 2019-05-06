30a114aa280630a8d359851e472a5c301938a36d
Alex Oachesu <alex.oachesu0001@stud.hkr.se>
2019-05-06 14:34:30 +0200

    create-ingredient-view-css
    fxml file
    css file

--- src/main/java/com/espressoshock/drinkle/controllers/app/IngredientList.java
+++ src/main/java/com/espressoshock/drinkle/controllers/app/IngredientList.java
@@ -1,6 +1,34 @@
 package com.espressoshock.drinkle.controllers.app;
 
 import com.espressoshock.drinkle.viewLoader.EventDispatcherAdapter;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 
 public class IngredientList extends EventDispatcherAdapter {
+    @FXML
+    private VBox vBoxIngredients;
+
+    @FXML
+    private Button btnCreate, btnAddToInventory, btnSearch, btnChoose1, btnChoose2;
+
+    @FXML
+    private CheckBox checkBoxAddToList, checkBoxLike;
+
+    @FXML
+    private MenuButton menuButtonBrands, menuButtonProducts, menuBtnGoTo;
+
+    @FXML
+    private ProgressBar progressBarAlcohol, progressBarPrice;
+
+    @FXML
+    private TextArea txtAreaDescription;
+
+    @FXML
+    private TextField  txtFieldPrice, txtFieldSearchOption, txtFieldVisualisations, txtFieldLike, txtFieldProductName, txtFieldSimilarWithProduct, txtFieldProductOneName, txtFieldProductTwoName, txtFieldAlcohol;
+
+    @FXML
+    private ImageView imgViewProduct, imgViewSimilarProduct1, imgViewSimilarProduct2;
+
 }
