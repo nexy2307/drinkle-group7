1b83a489c70784175c65020ce78cac5bd4fedac2
Alex Oachesu <alex.oachesu0001@stud.hkr.se>
2019-05-06 16:06:10 +0200

    create-view-ingredient

--- src/main/java/com/espressoshock/drinkle/controllers/app/IngredientList.java
+++ src/main/java/com/espressoshock/drinkle/controllers/app/IngredientList.java
@@ -1,6 +1,7 @@
 package com.espressoshock.drinkle.controllers.app;
 
 import com.espressoshock.drinkle.viewLoader.EventDispatcherAdapter;
+import com.espressoshock.drinkle.viewLoader.ViewMetadata;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
@@ -9,26 +10,22 @@ import javafx.scene.layout.VBox;
 public class IngredientList extends EventDispatcherAdapter {
     @FXML
     private VBox vBoxIngredients;
-
     @FXML
     private Button btnCreate, btnAddToInventory, btnSearch, btnChoose1, btnChoose2;
-
     @FXML
     private CheckBox checkBoxAddToList, checkBoxLike;
-
     @FXML
     private MenuButton menuButtonBrands, menuButtonProducts, menuBtnGoTo;
-
     @FXML
     private ProgressBar progressBarAlcohol, progressBarPrice;
-
     @FXML
     private TextArea txtAreaDescription;
-
     @FXML
     private TextField  txtFieldPrice, txtFieldSearchOption, txtFieldVisualisations, txtFieldLike, txtFieldProductName, txtFieldSimilarWithProduct, txtFieldProductOneName, txtFieldProductTwoName, txtFieldAlcohol;
-
     @FXML
     private ImageView imgViewProduct, imgViewSimilarProduct1, imgViewSimilarProduct2;
 
+    public void a(){
+      //  super.dispatchViewChangeRequest(ViewMetadata.);
+    }
 }
