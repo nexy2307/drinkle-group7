2662c6b8546aa8207024b6a3ffd5518132027127
Vincenzo Buono <vincebshock@gmail.com>
2019-05-08 21:20:23 +0200

    Account model annotation added

--- src/main/java/com/espressoshock/drinkle/models/Account.java
+++ src/main/java/com/espressoshock/drinkle/models/Account.java
@@ -1,65 +1,69 @@
 package com.espressoshock.drinkle.models;
 
 
-import java.util.ArrayList;
+import javax.persistence.*;
 import java.util.List;
 
+//JPA DB ANNOTATIONS //***********
+@Entity
+@Table(name = "account")
+//JPA DB ANNOTATIONS //***********
 public class Account implements IActionAccount{
-    private String accountID;
-    private String accountName;
-    private String accountPassword;
+    @Id
+    @Column(name="email")
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="picture_url")
+    private String pictureURL;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "person_ssn")
     private Person registered;
+
+    @Transient
     private List<Blueprint> blueprints;
 
-    public Account(String accountID, String accountName,String accountPassword,Person registered){
-        this.accountID = accountID;
-        this.accountName = accountName;
-        this.accountPassword = accountPassword;
-        this.registered = registered;
-        this.blueprints = new ArrayList<>();
-    }
-
-    public Account(String accountID, String accountName, String accountPassword, Person registered, List<Blueprint> blueprints) {
-        this.accountID = accountID;
-        this.accountName = accountName;
-        this.accountPassword = accountPassword;
+    public Account(String email, String password, String pictureURL, Person registered, List<Blueprint> blueprints) {
+        this.email = email;
+        this.password = password;
+        this.pictureURL = pictureURL;
         this.registered = registered;
         this.blueprints = blueprints;
     }
-
-    public String getAccountID(){return accountID;}
-
-    public String getAccountName(){return accountName;}
-
-    public String getAccountPassword(){return accountPassword;}
-
-    public Person getRegistered(){return registered;}
-
-    public Account(List<Blueprint> blueprints) {
-        this.blueprints = blueprints;
-    }
-
-    public void setAccountID(String accountID){this.accountID = accountID;}
-
-    public void setAccountName(String accountName){this.accountName = accountName;}
-
-    public void setAccountPassword(String accountPassword){this.accountPassword = accountPassword;}
-
-    public void setRegistered(Person registered){this.registered = registered;}
 
     public void setBlueprints(List<Blueprint> blueprints) {
         this.blueprints = blueprints;
     }
 
-    @Override
-    public String toString() {
-        return "Account{" +
-                "accountID='" + accountID + '\'' +
-                ", accountName='" + accountName + '\'' +
-                ", accountPassword='" + accountPassword + '\'' +
-                ", registered=" + registered +
-                ", blueprints=" + blueprints +
-                '}';
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Person getRegistered() {
+        return registered;
+    }
+
+    public List<Blueprint> getBlueprints() {
+        return blueprints;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPictureURL() {
+        return pictureURL;
+    }
+
+    public void setPictureURL(String pictureURL) {
+        this.pictureURL = pictureURL;
     }
 
     @Override
