2c90b916c837f2ab43ad23d07af647b7df710832
Vincenzo Buono <vincebshock@gmail.com>
2019-05-08 21:26:08 +0200

    JPAAccountDal generic type-safe implementation

---
+++ src/main/java/com/espressoshock/drinkle/daoLayer/entities/AccountDao.java
@@ -0,0 +1,7 @@
+package com.espressoshock.drinkle.daoLayer.entities;
+
+import com.espressoshock.drinkle.daoLayer.IDao;
+import com.espressoshock.drinkle.models.Account;
+
+public interface AccountDao extends IDao<String, Account> {
+}
